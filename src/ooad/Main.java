4f52b9dfcca166bc601b1b278e063e409b0bed1d
VINCENTIUS CHRISTIAN AJI WIANTO <vincentius.wianto@binus.ac.id>
2020-08-17 19:26:37 +0700

    yg tdi main nya kyknya error

--- src/ooad/Main.java
+++ src/ooad/Main.java
@@ -1,26 +1,23 @@
 package ooad;
 
 
-<<<<<<< HEAD
 import controllers.TaskController;
-=======
->>>>>>> refs/remotes/origin/master
+
 import controllers.UserController;
 
 public class Main {
 	
 
 	public Main() {
-<<<<<<< HEAD
+
 //		UserController.openProfileDisplay();
 //		UserController.openCreateUserDisplay();
 //		UserController.openAllUserDisplay();
 		TaskController.openCreateTaskForm();
-=======
+
 		//UserController.openProfileDisplay();
 		//UserController.openCreateUserDisplay();
 		UserController.openAllUserDisplay();
->>>>>>> refs/remotes/origin/master
 	}
 	
 	public static void main(String[] args) {
