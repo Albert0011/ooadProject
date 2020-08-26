6147d35b88c289cf20915485b730a3cc97bef295
Albert0011 <64696930+Albert0011@users.noreply.github.com>
2020-08-26 20:25:59 +0700

    albert task req

--- src/models/TaskRequest.java
+++ src/models/TaskRequest.java
@@ -1,9 +1,52 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class TaskRequest {
 
-	public TaskRequest() {
-		// TODO Auto-generated constructor stub
+	private String workerID;
+	private String supervisorID;
+	private String title;
+	private String note;
+	
+	public TaskRequest(String workerID, String supervisorID, String title, String note) {
+		super();
+		this.workerID = workerID;
+		this.supervisorID = supervisorID;
+		this.title = title;
+		this.note = note;
 	}
-
+	
+	
+	
+	
+	
+	public String getWorkerID() {
+		return workerID;
+	}
+	public void setWorkerID(String workerID) {
+		this.workerID = workerID;
+	}
+	public String getSupervisorID() {
+		return supervisorID;
+	}
+	public void setSupervisorID(String supervisorID) {
+		this.supervisorID = supervisorID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+	
+	
 }
